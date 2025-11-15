aabe1861e58962439ee69446066621415bb998ea
ZinYan <zinwaiyan2226@gmail.com>
2025-11-15 23:22:20 +0900

    템플릿 렌더링

--- posts/urls.py
+++ posts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
 from .views import *
 urlpatterns = {
-    path("",hello_world),
+    path("",posts_list),
 }
--- posts/views.py
+++ posts/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 from django.shortcuts import HttpResponse,render
 
-def hello_world(request):
-    return HttpResponse("Hello, World!")
+def posts_list(request):
+    return render(request,'posts_list.html')
